2c287a8620c42ce9657ac6b99db2fd1d5f273b8a
Matt Bishop <matt@The-Bishops-MacBook.local>
2014-07-31 21:50:46 +0100

    added stub spock test

---
+++ src/test/groovy/com/mattjbishop/droptest/PeopleResourceTest.groovy
@@ -0,0 +1,20 @@
+package com.mattjbishop.droptest
+
+import spock.lang.Specification
+
+/**
+ * Created by matt on 31/07/2014.
+ */
+class PeopleResourceTest extends Specification {
+    void setup() {
+
+    }
+
+    void cleanup() {
+
+    }
+
+    def "CreatePerson"() {
+        
+    }
+}
